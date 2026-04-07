da85abfd4a5762c42f9910d912a3bb0b177d2909
shoaib900 <rshoaib376@gmail.com>
2026-04-07 15:58:34 +0500

    update react js

--- src/Routes/About.js
+++ src/Routes/About.js
@@ -1,10 +1,39 @@
-import "./about.css";
+import { useEffect, useState } from "react";
+import style from "./about.module.css"
 
 const About = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then(res => res.json())
+      .then(json => setData(json))
+  }, []);
+
+
+  // console.log(data)
+
+
   return (
-    <div>
-        <h1 className='h1'>this is about page</h1>
-      
+    <div >
+      <h1 className='h1'>this is about page</h1>
+
+      <div className={style.container}>
+
+
+      {
+        data.map((item, index) => {
+          return (
+            <div className={style.card} >
+              <h3>{item.title}</h3>
+              <p> $ {item.price}</p>
+              <img src={item.images[0]} height={200} width={200} alt="" />
+            </div>
+          )
+        })
+      }
+
+      </div>
     </div>
   )
 }
--- src/Routes/Service.js
+++ src/Routes/Service.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Service = () => {
+  const [count,setCount] = useState(0)
+  const [errmsg,setErrmsg] = useState("");
+  
+  function dec(){
+    if(count === 0){
+      setCount(0)
+      setErrmsg("your value can't be in -ive...")
+    }else{
+      setCount(count - 1 )
+    }
+  }
   return (
     <div>
-        <h1> this is service page</h1>
+        <h1> this is service page {count} </h1>
+        <p style={{color:"tomato"}}>{errmsg}</p>
+
+        <button onClick={()=> {setCount(count + 1); setErrmsg("")}}>add</button> &nbsp;
+        <button onClick={dec}>Subtract</button>
+
+      <h1><pre>hello                      world</pre></h1>
       
     </div>
   )
